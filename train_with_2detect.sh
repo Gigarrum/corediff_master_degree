b24a2de5186b61b1d6c2ef9e6ed3761203adbfcc
Gigarrum <pbmausbach@hotmail.com>
2026-04-07 11:24:23 -0300

    update file with last test params

--- train_with_2detect.sh
+++ train_with_2detect.sh
@@ -1,8 +1,8 @@
 python main.py\
  --model_name corediff\
- --run_name test_training_with_2detect\
- --batch_size 3\
- --max_iter 150000\
+ --run_name test_training_with_2detect_36k_epochs_center_crop_only\
+ --batch_size 8\
+ --max_iter 360000\
  --train_dataset 2detect\
  --test_dataset 2detect\
  --test_id 9\
